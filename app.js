cd9543cce684e622c6af67f77be4f341bdb5e990
KCFindstr <changyuz@usc.edu>
2019-02-19 20:40:56 -0800

    finish lab

--- app.js
+++ app.js
@@ -41,4 +41,29 @@ app.get('/api/genres/:id', (request, response) => {
 		});
 });
 
+app.get('/api/artists', (request, response) => {
+	let connection = knex({
+		client: 'sqlite3',
+		connection: {
+			filename: 'database.sqlite'
+		}
+	});
+
+	let query = connection.select().from('artists');
+	if (request.query.filter) {
+		query = query.where('Name', 'like', `%${request.query.filter}%`)
+	}
+
+	query.then((artists) => {
+		artists = artists.map(obj => {
+			return {
+				id: obj.ArtistId.toString(),
+				name: obj.Name
+			}
+		});
+		response.json(artists);
+	});
+});
+
+
 app.listen(process.env.PORT || 8000);
